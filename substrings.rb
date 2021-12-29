7e625bab97cb19366f31f69d7be88005a94fbe34
gcerv93 <gcerv93@protonmail.com>
2021-12-29 10:07:27 -0600

    works for one word string

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,15 @@
 def substrings(string, dictionary)
+  result = Hash.new(0)
 
+  dictionary.each do |word| 
+    if string.include?(word)
+      result[word] += 1
+    end
+  end
+
+  result
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings('below', dictionary)
